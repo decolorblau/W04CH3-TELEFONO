e4294aa06b63b641daeed2d1d2e6a9ea4f33e7bb
Claudia Font <claudiafontmarques@gmail.com>
2021-10-21 19:05:34 +0200

    Repaired key test

--- src/components/Keyboard/Key/Key.test.js
+++ src/components/Keyboard/Key/Key.test.js
@@ -4,11 +4,23 @@ import Key from "./Key";
 describe("Given a Display component", () => {
   describe("When it receives 7", () => {
     test("Then it should return 7", () => {
-      const number = 7;
+      const key = {
+        textButton: 7,
+        className: "key",
+        disabled: "false",
+        actionOnClick: jest.fn(),
+      };
 
       const expectedNumber = 7;
 
-      render(<Key number={number} actionOnClick={() => {}} />);
+      render(
+        <Key
+          textButton={key.textButton}
+          className={key.className}
+          disabled={key.disabled}
+          actionOnClick={key.actionOnClick}
+        />
+      );
 
       const numberInput = screen.getByText(expectedNumber);
 
